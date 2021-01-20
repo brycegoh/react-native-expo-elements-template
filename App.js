c0a63583fcd2caebde510fead103213e79a61378
brycegoh <brycegoh96@gmail.com>
2021-01-20 22:58:46 +0800

    added firestore

--- App.js
+++ App.js
@@ -2,7 +2,32 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, ThemeProvider } from 'react-native-elements';
-import {apiKey} from './firebase'
+
+// firebase setup
+import firebaseConfig from './firebase'
+import * as firebase from 'firebase'
+import 'firebase/firestore';
+
+firebase.initializeApp(firebaseConfig);
+const store = firebase.firestore();
+
+
+// firebase test
+const triggerSet = () => {
+  store.collection("characters").doc("mario").set({
+    employment: "plumber",
+    outfitColor: "red",
+    specialAttack: "fireball"
+  })
+  return
+}
+
+const triggerGet = async () => {
+  const snapshot = await store.collection("characters").doc("mario").get()
+  const data = snapshot.data()
+  console.log( data.employment )
+  return
+}
 
 export default function App() {
 
@@ -11,7 +36,7 @@ export default function App() {
       <Text>Open up App.js to start working on your app!!</Text>
       <StatusBar style="auto" />
       <ThemeProvider>
-      <Button title={apiKey} />
+      <Button title="hello" onPress={triggerGet} />
     </ThemeProvider>
     </View>
   );
